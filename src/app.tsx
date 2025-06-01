9b5ee23ae323e04466768b3094706baf90155a60
Rahul Choudhury <rchoudhury63@gmail.com>
2025-06-01 22:54:01 +0530

    INIT COMMIT

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function App() {
+  return (
+    <div className="grid min-h-screen place-items-center">
+      <Button>Click Me</Button>
+    </div>
+  );
+}
