2be99a488c86302b39a6914ec750f7a7b85cf741
Rahul Choudhury <rchoudhury63@gmail.com>
2025-12-07 22:30:31 +0530

    feat: add global toast message for mutations

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,17 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { toast } from "sonner";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
+    },
+    mutations: {
+      onError: (error: Error) => {
+        toast.error(error.message);
+      },
     },
   },
 });
